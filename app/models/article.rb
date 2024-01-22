ca3f0607ae14ee91445d49e8d5a9f6d238bebe47
sungmia92 <157322275+sungmia92@users.noreply.github.com>
2024-01-22 06:05:21 -0500

    Jan 21

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+# File: app/models/article.rb
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+  
+    def self.search(query)
+      where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
+    end
+  end
+  
